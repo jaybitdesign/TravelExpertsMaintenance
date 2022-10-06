ef4b3118ee5bfba0ffd2ac0ae6c7e026b30247af
Bucky <jaybitdesign@gmail.com>
2022-10-05 21:05:38 -0600

    Added more comments

--- src/main/java/edu/sait/team1/travelexperts/travelexpertsmaintenance/database/DatabaseConnection.java
+++ src/main/java/edu/sait/team1/travelexperts/travelexpertsmaintenance/database/DatabaseConnection.java
@@ -1,16 +1,27 @@
 package edu.sait.team1.travelexperts.travelexpertsmaintenance.database;
 
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-
 import java.sql.*;
 
+/**
+ * DatabaseConnection.java
+ * Helper functions to access our database information more conveniently.
+ * James B., Ali H., Trevor P., Evan D.
+ * Fall 2022
+ */
 public class DatabaseConnection {
 
+    /** Define our connection string. */
     private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/travelexperts";
+
+    /** Define our username and password. */
     private static final String USER = "admin";
     private static final String PASS = "password";
 
+    /**
+     * -> getConnection
+     * @return Connection to our database by using our defined arguments.
+     * James B.
+     */
     private static Connection getConnection() {
         try {
             return DriverManager.getConnection(CONNECTION_STRING, USER, PASS);
@@ -20,23 +31,51 @@ public class DatabaseConnection {
         return null;
     }
 
+    /**
+     * -> executeStatement()
+     * Executes the prepared statement and returns the ResultSet.
+     * @param statement
+     * @return ResultSet from our executed SQL.
+     * James B.
+     */
     public static ResultSet executeStatement(String statement) {
         try {
+            /** Define a new PreparedStatement. */
             PreparedStatement preparedStatement = getConnection().prepareStatement(statement);
+
+            /** Execute the prepared statement. */
             preparedStatement.execute();
+
+            /** Return the result set */
             return preparedStatement.getResultSet();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * -> executeStatement()
+     * Executes the prepared statement after doing replacement and returns the ResultSet.
+     * @param statement
+     * @param parameters
+     * @return ResultSet from our executed SQL.
+     * James B.
+     */
     public ResultSet executeStatement(String statement, String[] parameters) {
         try {
+
+            /** Define a new PreparedStatement. */
             PreparedStatement preparedStatement = getConnection().prepareStatement(statement);
+
+            /** Iterate through the parameters and replace the correlating '?'. */
             for (int i = 0; i <= parameters.length; i++) {
                 preparedStatement.setString(i, parameters[i]);
             }
+
+            /** Execute the prepared statement. */
             preparedStatement.execute();
+
+            /** Return the result set */
             return preparedStatement.getResultSet();
         } catch (SQLException e) {
             throw new RuntimeException(e);
